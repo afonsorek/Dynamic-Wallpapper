efc86cb0a48c61204d951e853fb4cef167acfb5d
Afonso Rekbaim <afonso.rekbaim@pucpr.edu.br>
2024-03-11 18:23:19 -0300

    Original Creation

---
+++ dynamic-wallpapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dynamic-wallpapper
+//
+//  Created by Afonso Rekbaim on 11/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dynamic-wallpapper/dynamic_wallpapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_wallpapperApp.swift
+//  dynamic-wallpapper
+//
+//  Created by Afonso Rekbaim on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_wallpapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
